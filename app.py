6fe1799087077e49855c3d6006066a7cfc6f82fc
JoaoZati <joaozati@gmail.com>
2022-01-14 17:46:17 -0300

    4) add calc example

--- app.py
+++ app.py
@@ -8,5 +8,17 @@ def hello_word():
     return 'Hello Word'
 
 
+@app.route('/hithere')
+def hi_there():
+    return 'Hi there'
+
+
+@app.route('/calc')
+def calc():
+    # prepare a response
+    c = 200 * 3.14
+    return f'{str(c)}'
+
+
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000)
+    app.run(host='localhost', port=5000, debug=True)
